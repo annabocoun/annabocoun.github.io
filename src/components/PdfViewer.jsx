57a9120b1448754b593b1324a1c97aff9c34e05b
Misha <annabocoun@gmail.com>
2026-03-24 21:28:55 +0100

    Create PdfViewer component to view PDF with navigation

---
+++ src/components/PdfViewer.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+
+const PdfViewer = () => {
+  const [pageNumber, setPageNumber] = useState(1);
+  const totalPages = 5; // Update this according to your PDF
+
+  const nextPage = () => {
+    if (pageNumber < totalPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
+  return (
+    <div>
+      <Document file={'public/images/BOCOUN_PORTFOLIO_laika.pdf'}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <div>
+        <button onClick={prevPage} disabled={pageNumber <= 1}>Previous</button>
+        <button onClick={nextPage} disabled={pageNumber >= totalPages}>Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default PdfViewer;
